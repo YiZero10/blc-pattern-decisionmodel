6abc2667386c8287b8f84a02cf0363324a620216
YiZero <18350701710@163.com>
2022-12-03 23:11:47 +0800

    Initial commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,21 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# recommonmark is a python utility that allows markdown to be used within
+# Sphinx projects.
+# Installed version as per directive in docs/requirement.txt
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.md']
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
